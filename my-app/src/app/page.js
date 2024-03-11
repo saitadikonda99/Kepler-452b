8dc846bc76a86e32a79dbbdb06633558cb5cb2f4
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-03-11 15:37:21 +0530

    three eight nine sections added

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -5,6 +5,14 @@ import { useState } from 'react'
 // imports start here 
 import { faqData } from '@/data/FaqArray'
 import Footer from './Components/Footer/page'
+import Event from '@/app/Components/cards/Event'
+import { eventData } from '@/data/EventArray'
+
+
+// package imports start here
+import CountUp from 'react-countup';
+import VisibilitySensor from 'react-visibility-sensor';
+
 
 
 const page = () => {
@@ -24,7 +32,134 @@ const page = () => {
 
      <div className="HomeComponent">
         <div className="HomeComponent-in">
-            
+
+
+            {/* Section three starts here */}
+
+            <div className="three">
+                <div className="three-in">
+                    <div className="three-one">
+                        <div className="three-one-in">
+                            <h1>A Department without Boundaries</h1>
+                        </div>
+                    </div>
+                    <div className="three-two">
+                        <div className="three-two-in">
+                            <p>Designed to accommodate path-breaking ideas, problem-solving postulates, and artistic assertions, creating an environment that encourages innovation and experimentation</p>
+                        </div>
+                    </div>
+                    <div className="three-three">
+                        <div className="three-three-one">
+                            <div className="three-three-in-one count-cm">
+                                <h2>Collaborations</h2>
+                                <CountUp start={0} duration={4} end={100} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <h1 ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </div>
+                            <div className="three-three-in-two count-cm">
+                                <h2>Clubs</h2>
+                                <CountUp start={1} duration={4} end={33} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <h1 ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </div>
+                            <div className="three-three-in-three count-cm">
+                                <h2>Industry Mentors</h2>
+                                <CountUp start={1} duration={4} end={50} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <h1 ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </div>
+                            <div className="three-three-in-one count-cm">
+                                <h2>Actively Engaged Students</h2>
+                                <CountUp start={1} duration={4} end={6000} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <h1 ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </div>
+                            <div className="three-three-in-two count-cm">
+                                <h2>Events</h2>
+                                <CountUp start={1} duration={4} end={534} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <h1 ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </div>
+                            <div className="three-three-in-three count-cm">
+                                <h2>Projects</h2>
+                                <CountUp start={1} duration={4} end={104} redraw={true}>
+                                    {({ countUpRef, start }) => (
+                                        <VisibilitySensor onChange={start} delayedCall>
+                                        <h1 ref={countUpRef} />
+                                        </VisibilitySensor>
+                                    )}
+                                </CountUp>
+                            </div>
+                        </div>
+                        <div className="three-three-two">
+                             
+                        </div>
+                    </div>
+                    <div className="three-four">
+                        <div className="three-four-in">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+            {/* Section eight starts here */}
+
+            <div className="eight">
+                <div className="eight-in">
+                    <div className="eight-in-head">
+                        <h1>Upcoming Events</h1>
+                    </div>
+                    <div className="eight-one">
+                        <div className="eight-one-in">
+                            {eventData.map((event, index) => (
+                                <div className="eight-event-one Event-cm" key={index}>
+                                    <Event 
+                                        eventImage={event.image}
+                                        eventName={event.name}
+                                        eventDate={event.date}
+                                        eventVenue={event.venue}
+                                    />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Section nine starts here */}
+
+            <div className="nine">
+                <div className="nine-in">
+                    <div className="nine-one">
+                        <h1>Experience the power of community. Join us today and be a part of something truly special.</h1>
+                    </div>
+                    <div className="nine-two">
+                        <button>Join the Community</button>
+                    </div>
+                </div>
+            </div>
 
 
             {/* Section Twelve starts here */}
---
+++ my-app/src/data/EventArray.js
@@ -0,0 +1,29 @@
+export const eventData = [
+    { 
+        name: 'Event Name',
+        date: '13th Oct 2003',
+        image: 'https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/212/054/thumb/hack_and_snack_discord-icon.png?1665079646',
+        venue: 'Event Venue'
+    },
+
+    {
+        name: 'Event Name',
+        date: '13th Oct 2003',
+        image: 'https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/212/323/thumb/Hero_Title_Only.png?1667812364',
+        venue: 'Event Venue'
+    },
+
+    {
+        name: 'Event Name',
+        date: '13th Oct 2003',
+        image: 'https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/212/259/thumb/_1664290499001_Copy_of_Great_Uni_Hack_300_300_px.png?1664871039',
+        venue: 'Event Venue'
+    },
+
+    {
+        name: 'Event Name',
+        date: '13th Oct 2003',
+        image: 'https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/211/637/thumb/_1656603159928_300x300.png?1656724794',
+        venue: 'Event Venue'
+    },
+]
