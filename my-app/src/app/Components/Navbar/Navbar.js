760f884721ca096fd9591f8de4454389f9428fb1
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-03-11 21:53:32 +0530

    hero updated

---
+++ my-app/src/app/Components/Navbar/Navbar.js
@@ -0,0 +1,140 @@
+"use client"
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link as Scroll } from 'react-scroll';
+import Link from 'next/link';
+
+import './Navbar.css';
+import { imageConfigDefault } from 'next/dist/shared/lib/image-config';
+
+const Navbar = () => {
+
+    const [showDiv, setShowDiv] = useState(false);
+      
+        useEffect(() => {
+          const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+      
+            if (scrollPosition > window.innerHeight * 0.3) {
+              setShowDiv(true);
+            } else {
+              setShowDiv(false);
+            }
+          };
+      
+          window.addEventListener('scroll', handleScroll);
+      
+          return () => {
+            window.removeEventListener('scroll', handleScroll);
+          };
+    }, []);
+    
+
+
+    const handleScroll = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      }
+
+
+  return (
+    <div className="NavbarComponent">
+      <div className="NavbarComponent-in" id={showDiv ? 'show-color' : 'show-shadow'}>
+        <div className="Navbar-one">
+            { showDiv ? <h1 onClick={handleScroll}>Student Activity Center</h1> : <img src="https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/HeroVideo%2FOfficialSacLogo.png?alt=media&token=8a73bd93-832c-4d5d-819d-0e371d12b01c" alt="" /> }
+        </div>
+        <div className="Navbar-two">
+          <ul>
+            <li>
+              <Scroll
+                className='nav-scroll-links'
+                activeClass="active"
+                to="one"
+                spy={true}
+                smooth={true}
+                offset={-10}
+                duration={200}
+              >
+                About
+              </Scroll>
+            </li>
+            <li>
+              <Scroll
+                className='nav-scroll-links'
+                activeClass="active"
+                to="eight"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={200}
+              >
+                Clubs
+              </Scroll>
+            </li>
+            <li>
+              <Scroll
+                className='nav-scroll-links'
+                activeClass="active"
+                to="nine"
+                spy={true}
+                smooth={true}
+                offset={-50}
+                duration={200}
+              >
+                Blogs
+              </Scroll>
+            </li>
+            <li>
+              <Scroll
+                className='nav-scroll-links'
+                activeClass="Twelve"
+                to="news"
+                spy={true}
+                smooth={true}
+                offset={-60}
+                duration={200}
+              >
+                News
+              </Scroll>
+            </li>
+            <li>
+              <Scroll
+                className='nav-scroll-links'
+                activeClass="active"
+                to="ten"
+                spy={true}
+                smooth={true}
+                offset={-60}
+                duration={200}
+              >
+                Events
+              </Scroll>
+            </li>
+            <li>
+              <Scroll
+                className='nav-scroll-links'
+                activeClass="active"
+                to="Footer"
+                spy={true}
+                smooth={true}
+                offset={-60}
+                duration={200}
+              >
+                Leadership
+              </Scroll>
+            </li>
+            <li>
+              <Link href='https://sac-svr.vercel.app' target='_blank'>
+                  SVR
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,6 +1,6 @@
 "use client"
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 // imports start here 
 import { faqData } from '@/data/FaqArray'
@@ -8,15 +8,18 @@ import Footer from './Components/Footer/page'
 import Link from 'next/link'
 import Event from '@/app/Components/cards/Event'
 import { eventData } from '@/data/EventArray'
+import Navbar from '@/app/Components/Navbar/Navbar'
 
 
 // package imports start here
 import CountUp from 'react-countup';
 import VisibilitySensor from 'react-visibility-sensor';
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 
 
 const page = () => {
+
+
 
     const [expandedQuestions, setExpandedQuestions] = useState([])
     
@@ -28,11 +31,46 @@ const page = () => {
         }
     }
 
+    const handleExplore = () => {
+        window.scrollTo({
+            top: window.innerHeight,
+            behavior: "smooth"
+        })
+    }
+
 
   return (
 
      <div className="HomeComponent">
         <div className="HomeComponent-in">
+
+            {/* Section starts here */}
+
+            <div className="Navbar">
+                <Navbar />
+            </div>
+
+            {/* Section Hero starts here */}
+
+            <div className="hero">
+                <div className="hero-in">
+                    <video
+                        preload='auto'
+                        autoPlay
+                        loop
+                        muted
+                        style={{ width: "100%" }}
+                        src="https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/HeroVideo%2FnewVideo.mp4?alt=media&token=d193f8c5-f11a-4a44-9f08-f5ead58b33d8"
+                    />
+                    <div className="here-text">
+                        <h1>Student Activity Center</h1>
+                        <p className="animate-text">Empowering Tomorrow's Leaders Today</p>
+                    </div>
+                    <div className="hero-explore" onClick={handleExplore}>
+                        <p>Explore SAC</p> 
+                    </div>
+                </div>
+            </div>
 
               {/* Section three starts here */}
 
