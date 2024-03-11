d5ebe8e8cab1078d9d1f2b7a7b90abb08978c5f7
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-03-11 12:40:47 +0530

    faq's added

---
+++ my-app/src/data/FaqArray.js
@@ -0,0 +1,23 @@
+export const faqData = [
+    {
+      question: 'What is Student Activity Center at Koneru Lakshmaiah Education Foundation?',
+      answer: 'Student Activity Center is the epicenter of innovation and personal growth. The sole existence of this is to show a student the true capabilities they posses beyond academics. SAC fosters and supports ideas with 5 major parameters Technical, Cultural & Sports, Outreach Activities & Societies, Innovation & Incubation, Health & Wellness. We at SAC have clubs who aim to progress in these domains.',
+    },
+    {
+      question: 'How can I be a club member?',
+      answer: 'To be a club member visit your interested club page on the website and have an information of your club team, then collect your membership registration form from your respective club head by visiting the SAC Hall and post approval you are a registered member of your club. ',
+    },
+    {
+      question: 'Can I establish a club at SAC?',
+      answer: 'Yes, we welcome you to establish a club at SAC. Any club that has a clarity of its objective, activities and workflow with a strong foundation of their club prerequisites can establish their club at SAC. Initially the club will be under a probation period and after successfully analyzing the club operations and dedication SAC will declare it as permanent and support/provide for the club. Adhering to the SAC policies and university norms goes unsaid.',
+    },
+    {
+      question: 'What is the Student Council at SAC?',
+      answer: 'The Student Council at SAC is a team of highly sincere and dedicated students consisting their vast expertise in their respective domains. The Council is responsible to strategize and execute ideas/plans for the club development, student welfare, institutional growth. It is the backbone and the strongest support for the students of the University.',
+    },
+    {
+      question: 'Are first year allowed in SAC?',
+      answer: 'First years freshers they are always welcomed. SAC is a place where your talents flourish and also you gain new skills with coming time.',
+    },
+
+];
