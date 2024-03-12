1de7fe5d809a9d7b84ba2c309609e3e06fc9d4d9
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-03-12 16:10:37 +0530

    posters updated

--- my-app/src/data/EventArray.js
+++ my-app/src/data/EventArray.js
@@ -7,23 +7,23 @@ export const eventData = [
     },
 
     {
-        name: ' Yoga for flexibility',
+        name: 'Yoga for flexibility',
         date: '14th March 2024',
         image: 'https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/Posters%2FYoga.png?alt=media&token=3bb9b888-be78-4f5f-87cd-5a6a0570bd6e',
         venue: 'R 301B'
     },
 
     {
-        name: 'Event Name',
-        date: '13th Oct 2003',
-        image: 'https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/212/259/thumb/_1664290499001_Copy_of_Great_Uni_Hack_300_300_px.png?1664871039',
-        venue: 'Event Venue'
+        name: 'Jira Fundamentals',
+        date: '26th March 2024',
+        image: 'https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/Posters%2F1%20(1).png?alt=media&token=3b7b4789-8acd-4619-98cc-bce685377499',
+        venue: 'R301A'
     },
 
     {
-        name: 'Event Name',
-        date: '13th Oct 2003',
-        image: 'https://s3.amazonaws.com/assets.mlh.io/events/splashes/000/211/637/thumb/_1656603159928_300x300.png?1656724794',
-        venue: 'Event Venue'
+        name: 'NodeJS:Authentication',
+        date: '21st March 2024',
+        image: 'https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/Posters%2F2%20(1).png?alt=media&token=7b444e5c-4f93-4b39-8008-38558ec2c0f1',
+        venue: 'SAC Hall'
     },
 ]
