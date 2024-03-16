6a2c9677ee2062f778479850d7b2827c5b479199
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-03-16 15:31:04 +0530

    navbar addded

--- my-app/src/app/clubs/TEC/ZeroOneCodeClub/page.js
+++ my-app/src/app/clubs/TEC/ZeroOneCodeClub/page.js
@@ -1,9 +1,12 @@
 import React from 'react'
 
 
+import './page.css'
+
 // imports start here 
-import Navbar from '../../components/navbar/page'
+import Navbar from '../../components/navbar/ClubNavbar'
 import Footer from '../../../Components/Footer/page'
+import { ClubsArray } from '../../../../data/ClubsArray'
 
 
 const page = () => {
@@ -11,10 +14,16 @@ const page = () => {
         <div className="ClubPageComponent">
             <div className="ClubPageComponent-in">
                 <div className="ClubPageNavbar">
-                    <Navbar ClubName="ZeroOne CodeClub"/>
+                    <Navbar ClubName={ClubsArray[0].clubName} ClubImg={ClubsArray[0].clubImg} />
                 </div>
                 <div className="ClubPage">
                     <div className="ClubPage-in">
+
+                        <div className="Club-one">
+                            <div className="Club-one-one">
+                                <img src={ClubsArray[0].heroImg} alt="" />
+                            </div>
+                        </div>
 
                     </div>
                 </div>  
