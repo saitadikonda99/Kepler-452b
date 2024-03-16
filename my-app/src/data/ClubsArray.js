38b0b696c4f11eca39842ba165ec8018c74fe338
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-03-16 15:38:07 +0530

    Clubs data added

---
+++ my-app/src/data/ClubsArray.js
@@ -0,0 +1,23 @@
+export const ClubsArray = [
+    
+    // ZeroOne CodeClub
+    {
+        clubName: "ZeroOne CodeClub",
+        clubImg: "https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/HeroVideo%2FOfficialSacLogo.png?alt=media&token=8a73bd93-832c-4d5d-819d-0e371d12b01c",
+        heroImg: "https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/Clubs%2FImg.png?alt=media&token=51892429-a238-4ff4-a31c-6b54c1b77f6d"
+    },
+
+    // WebApps 
+    {
+        clubName: "WebApps",
+        clubImg: "https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/HeroVideo%2FOfficialSacLogo.png?alt=media&token=8a73bd93-832c-4d5d-819d-0e371d12b01c",
+        clubPage: "WebApps"
+    },
+
+    // CyberSecurity
+    {
+        clubName: "CyberSecurity",
+        clubImg: "https://firebasestorage.googleapis.com/v0/b/sacwebsite-8d0b5.appspot.com/o/HeroVideo%2FOfficialSacLogo.png?alt=media&token=8a73bd93-832c-4d5d-819d-0e371d12b01c",
+        clubPage: "CyberSecurity"
+    },
+]
