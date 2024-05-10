598a39c26db4332b3f49a1e8d0f91826ed47484c
Tadikonda Sai Manikanta <97383279+saitadikonda99@users.noreply.github.com>
2024-05-10 22:32:56 +0530

    Update queries.sql

--- my-app/src/config/queries.sql
+++ my-app/src/config/queries.sql
@@ -1,14 +1,1 @@
-INSERT INTO users (username, password, email, role) 
-VALUES 
-(2200030805, 'sai@1234', '220030805@kluniversity.in', 'Admin'),
-(2100031817, 'deepak@1234', '2100031817@kluniversity.in', 'Admin'),
-(2200031219, 'pavan@1234', '2200031219@kluniversity.in', 'Admin')
 
-
-INSERT INTO users (username, password, email, role) 
-VALUES 
-(2200033327, 'rekha@1234', '2200033327@kluniversity.in', 'President'),
-(2100033327, 'kavya@1234', '2100033327@kluniversity.in', 'President'),
-(2200030123, 'dinesh@1234', '2200030123@kluniversity.in', 'President')
-
-INSERT INTO events (eventLink, eventName, eventDate, eventTime, eventCardNum) VALUES (?, ?, ?, ?, ?)
