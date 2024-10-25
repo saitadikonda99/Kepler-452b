0677f361197204d716994eb5650e271b50d8e8b0
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-10-25 16:53:17 +0530

    made some changes in dashboard

--- my-app/src/app/(pages)/components/footer/page.js
+++ my-app/src/app/(pages)/components/footer/page.js
@@ -6,7 +6,7 @@ const page = () => {
   return (
         <div className="AdminFooterComponent">
             <div className="AdminFooterComponent-in">
-                <p>2023 KLEF - Student Activity Center | Designed & Developed by Deepak Reddy Gathpa & Tadikonda Sai Manikanta</p>
+                <p>2023 KLEF - Student Activity Center | Designed & Developed by ZeroOne CodeClub</p>
             </div>
         </div>
   )
