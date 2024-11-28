c3d4419536b09e4397b01ca37ab86390321609a6
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-11-29 00:20:07 +0530

    club details frontend updated

---
+++ my-app/src/app/(pages)/courseDetails/page.js
@@ -0,0 +1,152 @@
+"use client";
+
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import toast from "react-hot-toast";
+import Link from "next/link";
+import Navbar from "../sil/Navbar";
+import Footer from "../components/footer/page";
+
+import { IoIosArrowUp } from "react-icons/io";
+import { IoIosArrowDown } from "react-icons/io";
+
+import "./page.css";
+
+import Loader from "../../animation/loader";
+
+const page = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [courseData, setCourseData] = useState([]);
+  const [openStates, setOpenStates] = useState({});
+
+  const toggleOpen = (clubName) => {
+    setOpenStates((prevStates) => ({
+      ...prevStates,
+      [clubName]: !prevStates[clubName],
+    }));
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("/api/getCourses");
+
+        if (response.status === 200) {
+          setCourseData(response.data);
+          setIsLoading(false);
+        } else {
+          console.error("Error fetching data");
+          toast.error("Failed to load data");
+        }
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        toast.error("Failed to load data");
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(courseData);
+
+  return isLoading ? (
+    <Loader />
+  ) : (
+    <div className="CourseComponent">
+      <div className="CourseComponent-in">
+        <div className="Course-Nav">
+          <Navbar />
+        </div>
+        <div className="Course-one">
+          <div className="Course-one-in">
+            <div className="Course-one-one">
+              <h1>All Clubs Course Details</h1>
+            </div>
+
+            {Object.entries(
+              courseData.reduce((acc, course) => {
+                if (!acc[course.club_name]) {
+                  acc[course.club_name] = {
+                    clubDomain: course.club_domain,
+                    courses: [],
+                  };
+                }
+                acc[course.club_name].courses.push(course);
+                return acc;
+              }, {})
+            ).map(([clubName, { clubDomain, courses }]) => {
+              const slots = courses.some((course) => course.course_slots - course.register_students > 0);
+
+              return (
+                <div key={clubName} className="Course-one-two">
+                  <div className="Course-one-two-in">
+                    <div className="Co-one" onClick={() => toggleOpen(clubName)}>
+                      <div className="Co-one-one">
+                        <p>{clubName}</p>
+                      </div>
+                      <div className="Co-one-two">
+                        <div className="Co-one-two-in-one">
+                          <p>Domain: {clubDomain}</p>
+                        </div>
+                        <div className="Co-one-two-in-two">
+                          <p className={slots ? "course-available" : "course-not-available"}>{slots ? "Slots Available" : "No Slots Available"}</p>
+                        </div>
+                      </div>
+                      <div className="Co-one-three">
+                        {openStates[clubName] ? (
+                          <IoIosArrowUp />
+                        ) : (
+                          <IoIosArrowDown />
+                        )}
+                      </div>
+                    </div>
+                    {openStates[clubName] && (
+                      <div className="Co-two">
+                        <div className="Co-two-in">
+                          <table>
+                            <thead>
+                              <tr>
+                                <th>Course Name</th>
+                                <th>Course Code</th>
+                                <th>Course Slots</th>
+                                <th>Course Offered Year</th>
+                                <th>Course Handout</th>
+                                <th>Registered Students</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              {courses.map((course) => (
+                                <tr key={course.course_id}>
+                                  <td>{course.course_name}</td>
+                                  <td>{course.course_code}</td>
+                                  <td>{course.course_slots}</td>
+                                  <td>{course.course_year}</td>
+                                  <td>
+                                      <Link href={course.course_handout}>
+                                          <p>Handout</p>
+                                      </Link>
+                                  </td>
+                                  <td>{course.register_students}</td>
+                                </tr>
+                              ))}
+                            </tbody>
+                          </table>
+                        </div>
+                      </div>
+                    )}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div className="Course-footer">
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
