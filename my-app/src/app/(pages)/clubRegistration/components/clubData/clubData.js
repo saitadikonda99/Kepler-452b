3827d136a4ca0b639132dc93d993186a0a531045
sai tadikonda <saitadikonda@sais-MacBook-Air.local>
2024-12-11 14:18:25 +0530

    student data pages updated on admin

--- my-app/src/app/(pages)/clubRegistration/components/clubData/clubData.js
+++ my-app/src/app/(pages)/clubRegistration/components/clubData/clubData.js
@@ -270,7 +270,7 @@ const page = ({ data, setData }) => {
             </div>
 
             <div className="ClubData-four-one-two crInput">
-              <label htmlFor="erpReferenceNumber">ERP Payment Receipt Number/Order Number:</label>
+              <label htmlFor="erpReferenceNumber">ERP Payment Gateway Reference Number:</label>
               <input
                 type="text"
                 placeholder="please provide correct reference number it will be verified by SAC"
