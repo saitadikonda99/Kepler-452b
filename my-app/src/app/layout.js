7555271147b1b8d7e8ae25a65ccaca0f2c2121fe
saitadikonda99 <saitadikonda.tech@gmail.com>
2025-01-28 14:42:43 +0530

    build error solved & updated

--- my-app/src/app/layout.js
+++ my-app/src/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "KL University SAC",
@@ -10,10 +7,9 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
   return (
     <html lang="en">
-        <body className={inter.className}>
+        <body className="font-sans">
             <Toaster position="top-right" />
             {children}
         </body>
